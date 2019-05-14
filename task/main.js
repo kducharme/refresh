8f53b0b3ace2d6712675d6445a7143f94ddf08a1
Kyle Ducharme <ducharme.kyle@gmail.com>
2019-05-14 09:19:57 -0400

    js working

--- task/main.js
+++ task/main.js
@@ -1,1 +1,9 @@
-console.log('hey')
+const content = document.querySelector('#content');
+
+const createPage = (content) => {
+    const addNew = document.createElement('button')
+    addNew.textContent = 'Hey'
+    content.appendChild(addNew)
+}
+
+createPage(content)
