167d8e35ba3119695abd165817e146ccb89753af
Kyle Ducharme <ducharme.kyle@gmail.com>
2019-05-16 10:46:09 -0400

    completed exercise

--- nss/chapter1/app.js
+++ nss/chapter1/app.js
@@ -1,10 +1,9 @@
 const allNum = [];
 
 const print = () => {
-    const content = document.querySelector('#content');
     for (i = 0; i < 10; i++) {
         allNum.push(Math.random() * 10);
-        content.textContent = `${allNum[i]}       `;
+        content.textContent += `${allNum[i]}`;
     }
     console.log(content)
 }
