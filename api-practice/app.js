885faecce4289d04f6e680d13f1ed5b80098c1cf
Kyle Ducharme <ducharme.kyle@gmail.com>
2019-05-18 21:06:53 -0400

    full todo received

--- api-practice/app.js
+++ api-practice/app.js
@@ -1,25 +1,38 @@
+// Gets todo data
 const getTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos/')
-      .then(response => response.json())
-      .then(todos => displayTodos(todos))
-    }
-    
-    const displayTodos = (todos) => {
-        const printArea = document.querySelector('#content');
-        todos.forEach((todo) => {
-            const structure = document.createElement('div');
-            const user = getUser(todo.userId);
-            console.log(user)
-        })
-    }
-    
-const getUser = (id) => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then(response => response.json())
-        .then(user => {
-            return user;
-        })
+        .then(todos => getUserData(todos))
+}
+
+// Gets full user data
+const getUserData = (todos) => {
+    todos.forEach((todo) => {
+        const structure = document.createElement('div');
+        fetch(`https://jsonplaceholder.typicode.com/users/${todo.userId}`)
+            .then(response => response.json())
+            .then(user => {
+                todo.user = user;
+            })
+        generateTodoContent(todo)
+    })
+}
+
+// Generates printable todo
+const genereateTodoContent = (data) => {
+    const todo = document.createElement('div');
+    let todoTitle = data.title,
+        todoStatus = data.completed,
+        todoUserName = data.user.name,
+        todoUserEmail = data.user.email;
+    
+    
+
 }
 
 
+// Prints todo to DOM
+const printTodo = (todo) => {
+    const printArea = document.querySelector('#content');
+}
 getTodos();
