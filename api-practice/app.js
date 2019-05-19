424ab4180c56fadb8973590a24dba54b2be1d4eb
Kyle Ducharme <ducharme.kyle@gmail.com>
2019-05-18 21:21:00 -0400

    todos printing

--- api-practice/app.js
+++ api-practice/app.js
@@ -13,26 +13,35 @@ const getUserData = (todos) => {
             .then(response => response.json())
             .then(user => {
                 todo.user = user;
+                generateTodoContent(todo)
             })
-        generateTodoContent(todo)
     })
 }
 
 // Generates printable todo
-const genereateTodoContent = (data) => {
+const generateTodoContent = (data) => {
     const todo = document.createElement('div');
-    let todoTitle = data.title,
-        todoStatus = data.completed,
-        todoUserName = data.user.name,
-        todoUserEmail = data.user.email;
-    
-    
+    let todoTitle = createParagraph(data.title),
+        todoStatus = createParagraph(data.completed),
+        todoUserName = createParagraph(data.user.name),
+        todoUserEmail = createParagraph(data.user.email);
+    todo.appendChild(todoTitle);
+    todo.appendChild(todoStatus);
+    todo.appendChild(todoUserName);
+    todo.appendChild(todoUserEmail);
+    printTodo(todo)
+}
 
+const createParagraph = (todoData) => {
+    const p = document.createElement('p');
+    p.textContent = todoData;
+    return p;
 }
 
 
 // Prints todo to DOM
 const printTodo = (todo) => {
     const printArea = document.querySelector('#content');
+    printArea.appendChild(todo)
 }
 getTodos();
